266500723ba725944cab9de933a585eab85b2317
ZainR92 <rahjeh@gmail.com>
2023-05-20 14:23:07 +0200

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,10 +86,12 @@ const scrollActive = () => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute("id"),
-            sectionsClass = document.querySelector(".nav__menu a [href*=" + sectionId + "]")
+            sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]")
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             sectionsClass.classList.add("active-link")
+        } else {
+            sectionsClass.classList.remove("active-link")
         }
     })
 }

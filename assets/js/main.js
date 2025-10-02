dbe8b558fc30c611dc11bbeb09579f8a462845d3
Zain <69877681+Rahjeh92@users.noreply.github.com>
2025-10-02 16:02:34 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,29 @@ const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
     navClose = document.getElementById("nav-close")
 
-/* ===== MENU SHOW ==== */
-/* Validate if constant exists */
+// Show Menu (for mobile)
 if (navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu")
     })
 }
 
-/* ===== MENU HIDDEN ==== */
-/* Validate if constant exists */
+// Hide Menu (for mobile)
 if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
     })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== REMOVE MENU MOBILE WHEN LINK CLICK ===============*/
 const navLink = document.querySelectorAll(".nav__link")
-
-const linkAction = () => {
-    const navMenu = document.getElementById("nav-menu")
-    // When we click on each nav__link, we remove the show-menu class
+navLink.forEach(n => n.addEventListener("click", () => {
     navMenu.classList.remove("show-menu")
-}
-navLink.forEach(n => n.addEventListener("click", linkAction))
+}))
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+/*=============== CHANGE BACKGROUND HEADER ON SCROLL ===============*/
 const blurHeader = () => {
     const header = document.getElementById("header")
-    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
     this.scrollY >= 50 ? header.classList.add("blur-header")
         : header.classList.remove("blur-header")
 }
@@ -40,37 +33,27 @@ window.addEventListener("scroll", blurHeader)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById("contact-form"),
-    constantMessage = document.getElementById("contact-message")
+    contactMessage = document.getElementById("contact-message")
 
 const sendEmail = (e) => {
     e.preventDefault()
 
-    // serviceID - templateID - #form - publicKey
     emailjs.sendForm("service_ai9zjfc", "template_641w81u", "#contact-form", "aQ1eShe--kV8MfRvs")
         .then(() => {
-            // Show sent message
-            constantMessage.textContent = "Message sent successfully ✅"
-
-            // Remove message after five seconds
-            setTimeout(() => {
-                contactMessage.textContent = ""
-            }, 5000)
-
-            // Clear input fields
+            // Success message
+            contactMessage.textContent = "Message sent successfully ✅"
+            setTimeout(() => { contactMessage.textContent = "" }, 5000)
             contactForm.reset()
-
         }, () => {
-            // Show error message
-            constantMessage.textContent = "Message not sent (service error) ❌"
+            // Error message
+            contactMessage.textContent = "Message not sent (service error) ❌"
         })
 }
+if (contactForm) contactForm.addEventListener("submit", sendEmail)
 
-contactForm.addEventListener("submit", sendEmail)
-
-/*=============== SHOW SCROLL UP ===============*/
+/*=============== SHOW SCROLL UP BUTTON ===============*/
 const scrollUP = () => {
     const scrollUP = document.getElementById("scroll-up")
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with scrollup
     this.scrollY >= 350 ? scrollUP.classList.add("show-scroll")
         : scrollUP.classList.remove("show-scroll")
 }
@@ -81,28 +64,28 @@ const sections = document.querySelectorAll("section[id]")
 
 const scrollActive = () => {
     const scrollY = window.pageYOffset
-
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute("id"),
-            sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]")
+            sectionLink = document.querySelector(".nav__menu a[href*=" + sectionId + "]")
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add("active-link")
+            sectionLink.classList.add("active-link")
         } else {
-            sectionsClass.classList.remove("active-link")
+            sectionLink.classList.remove("active-link")
         }
     })
 }
 window.addEventListener("scroll", scrollActive)
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: "top",
     distance: "60px",
     duration: 2500,
-    delay: 400.
-    // reset: true // Animations repeat
+    delay: 400
+    // reset: true // Uncomment if you want animations to repeat
 })
 
 sr.reveal(".home__data, .home__social, .contact__container, .footer__container")
